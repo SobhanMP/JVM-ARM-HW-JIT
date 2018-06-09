99299bedd796766763513fd275f35df650a74fbc
Sobhan Mohammadpour <www.sobhan.mohammadpour@gmail.com>
2018-06-09 12:40:33 +0430

    fix instruction identation

--- instructions/ldfkjg.s
+++ instructions/ldfkjg.s
@@ -1,153 +1,153 @@
-area start, code
-  export StartHere
+	area start, code
+	export StartHere
 StartHere
-  MRC p15, 0, r0, c1, c1, 2
+	MRC p15, 0, r0, c1, c1, 2
 
-  ORR r0, r0, #2_11 << 10 ; enable fpu
+	ORR r0, r0, #2_11<<10 ; enable fpu
 
-  MCR p15, 0, r0, c1, c1, 2
+	MCR p15, 0, r0, c1, c1, 2
 
-  LDR r0, =(0xF << 20)
+	LDR r0, =(0xF << 20)
 
-  MCR p15, 0, r0, c1, c0, 2
+	MCR p15, 0, r0, c1, c0, 2
 
-  MOV r3, #0x40000000
+	MOV r3, #0x40000000
 
-  VMSR FPEXC, r3
+	VMSR FPEXC, r3
 
-  import __main
-  b __main
+	import __main
+	b __main
 
-  area mycode, code
-  export myasm
+	area mycode, code
+	export myasm
 myasm
 
 
-  ;#fconst_0
-  vsub.f32 s0, s0, s0
-  add sp, #4
-  vstr.f32 s0, [sp]
+	;#fconst_0
+	vsub.f32 s0, s0, s0
+	add sp, #4
+	vstr.f32 s0, [sp]
 
-  ;#fconst_1
-  vmov.f32 s0, #1
-  add sp, #4
-  vstr.f32 s0, [sp]
+	;#fconst_1
+	vmov.f32 s0, #1
+	add sp, #4
+	vstr.f32 s0, [sp]
 
-  ;#fconst_2
-  vmov.f32 s0, #2
-  add sp, #4
-  vstr.f32 s0, [sp]
+	;#fconst_2
+	vmov.f32 s0, #2
+	add sp, #4
+	vstr.f32 s0, [sp]
 
-  ;#dconst_0
-  vsub.f64 d0, d0, d0
-  add sp, #8
-  vstr.f64 d0, [sp]
+	;#dconst_0
+	vsub.f64 d0, d0, d0
+	add sp, #8
+	vstr.f64 d0, [sp]
 
-  ;#dconst_1
-  vmov.f64 d0, #1
-  add sp, #8
-  vstr.f64 d0, [sp]
+	;#dconst_1
+	vmov.f64 d0, #1
+	add sp, #8
+	vstr.f64 d0, [sp]
 
-  ;#fdiv check who to devide by who
+	;#fdiv check who to devide by who
 
-  vldr.f32 s0, [sp]
-  sub sp, #4
-  vldr.f32 s1, [sp]
-  vdiv.f32 s2,s1,s0
-  vstr.f32 s2, [sp]
+	vldr.f32 s0, [sp]
+	sub sp, #4
+	vldr.f32 s1, [sp]
+	vdiv.f32 s2,s1,s0
+	vstr.f32 s2, [sp]
 
-  ;#fmul
-  vldr.f32 s0, [sp]
-  sub sp, #4
-  vldr.f32 s1, [sp]
-  vmul.f32 s2,s1,s0
-  vstr.f32 s2, [sp]
+	;#fmul
+	vldr.f32 s0, [sp]
+	sub sp, #4
+	vldr.f32 s1, [sp]
+	vmul.f32 s2,s1,s0
+	vstr.f32 s2, [sp]
 
-  ;#fadd
-  vldr.f32 s0, [sp]
-  sub sp, #4
-  vldr.f32 s1, [sp]
-  vadd.f32 s2,s1,s0
-  vstr.f32 s2, [sp]
-
-
-  ;#fload_0
-  vldr.f32 s0, [fp]
-  add sp, #4
-  vstr.f32 s0, [sp]
+	;#fadd
+	vldr.f32 s0, [sp]
+	sub sp, #4
+	vldr.f32 s1, [sp]
+	vadd.f32 s2,s1,s0
+	vstr.f32 s2, [sp]
 
 
-  ;#fload_1
-  vldr.f32 s0, [fp, #4]
-  add sp, #4
-  vstr.f32 s0, [sp]
-
-  ;#float_2
-  vldr.f32 s0, [fp, #8]
-  add sp, #4
-  vstr.f32 s0, [sp]
-
-  ;#float_3
-  vldr.f32 s0, [fp, #12]
-  add sp, #4
-  vstr.f32 s0, [sp]
-
-  ;#fneg
-  vldr.f32 s1, [sp]
-  vneg.f32 s0, s1
-  vstr.f32 s0, [sp]
+	;#fload_0
+	vldr.f32 s0, [fp]
+	add sp, #4
+	vstr.f32 s0, [sp]
 
 
-  ;#frem
-  add sp, #4
-  vldm.f32 sp!, {s0, s1}
-  vdiv.f32 s2,s0,s1
-  vcvt.s32.f32 s4,s2
-  vcvt.f32.s32 s2,s4
-  vmul.f32 s3,s2,s1
-  vsub.f32 s4,s0,s3
+	;#fload_1
+	vldr.f32 s0, [fp, #4]
+	add sp, #4
+	vstr.f32 s0, [sp]
 
-  vstr.f32 s4, [sp]
+	;#float_2
+	vldr.f32 s0, [fp, #8]
+	add sp, #4
+	vstr.f32 s0, [sp]
 
-  ;#f2d
-  vldr.f32 s0, [sp]
-  vcvt.f64.f32 d1, s0
-  vstr.f64 d1, [sp]
-  add sp, #4
+	;#float_3
+	vldr.f32 s0, [fp, #12]
+	add sp, #4
+	vstr.f32 s0, [sp]
 
-  ;#f2i
-  vldr.f32 s0, [sp]
-  vcvt.s32.f32 s1, s0
-  vstr.f32 s1, [sp]
-
-  ;#d2f
-  vldr.f64 d0, [sp]
-  vcvt.f32.f64 s0, d0
-  vstr.f32 s0, [sp]
-
-  ;#dadd
-  vldr.f64 d0, [sp]
-  sub sp, #8
-  vldr.f64 d1, [sp]
-  vadd.f64 d2,d1,d0
-  vstr.f64 d2, [sp]
-
-  ;#dsub
-  vldr.f64 d0, [sp]
-  sub sp, #8
-  vldr.f64 d1, [sp]
-  vsub.f64 d2,d1,d0
-  vstr.f64 d2, [sp]
+	;#fneg
+	vldr.f32 s1, [sp]
+	vneg.f32 s0, s1
+	vstr.f32 s0, [sp]
 
 
+	;#frem
+	add sp, #4
+	vldm.f32 sp!, {s0, s1}
+	vdiv.f32 s2,s0,s1
+	vcvt.s32.f32 s4,s2
+	vcvt.f32.s32 s2,s4
+	vmul.f32 s3,s2,s1
+	vsub.f32 s4,s0,s3
+
+	vstr.f32 s4, [sp]
+
+	;#f2d
+	vldr.f32 s0, [sp]
+	vcvt.f64.f32 d1, s0
+	vstr.f64 d1, [sp]
+	add sp, #4
+
+	;#f2i
+	vldr.f32 s0, [sp]
+	vcvt.s32.f32 s1, s0
+	vstr.f32 s1, [sp]
+
+	;#d2f
+	vldr.f64 d0, [sp]
+	vcvt.f32.f64 s0, d0
+	vstr.f32 s0, [sp]
+
+	;#dadd
+	vldr.f64 d0, [sp]
+	sub sp, #8
+	vldr.f64 d1, [sp]
+	vadd.f64 d2,d1,d0
+	vstr.f64 d2, [sp]
+
+	;#dsub
+	vldr.f64 d0, [sp]
+	sub sp, #8
+	vldr.f64 d1, [sp]
+	vsub.f64 d2,d1,d0
+	vstr.f64 d2, [sp]
 
 
-  ;faload
-;  dmda sp!, {r0, r1}
-;  mov r1, r1, ASL#2
-;  vldr.f32 s0, [r0, r1]
-;  vstmib.f32 sp!, {s0}
 
-  bx lr
 
-  end
+	;faload
+;	dmda sp!, {r0, r1}
+;	mov r1, r1, ASL#2
+;	vldr.f32 s0, [r0, r1]
+;	vstmib.f32 sp!, {s0}
+
+	bx lr
+
+	end
