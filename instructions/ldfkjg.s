209b5a6551900b222eb9005fec05196013e49503
Rozhin <nobahari.r@gmail.com>
2018-06-09 12:30:05 +0430

    initialized

--- instructions/ldfkjg.s
+++ instructions/ldfkjg.s
@@ -3,11 +3,11 @@ area start, code
 StartHere
   MRC p15, 0, r0, c1, c1, 2
 
-  ORR r0, r0, #2_11«10 ; enable fpu
+  ORR r0, r0, #2_11 << 10 ; enable fpu
 
   MCR p15, 0, r0, c1, c1, 2
 
-  LDR r0, =(0xF « 20)
+  LDR r0, =(0xF << 20)
 
   MCR p15, 0, r0, c1, c0, 2
 
