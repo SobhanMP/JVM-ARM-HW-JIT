7abfeacfef6c426778dcd83bd36a4d3cbd52395b
Sobhan Mohammadpour <www.sobhan.mohammadpour@gmail.com>
2018-06-09 12:43:30 +0430

    call myasm function in C file

--- instructions/test.c
+++ instructions/test.c
@@ -11,7 +11,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern void myasm(void);
+
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	myasm();
 	return EXIT_SUCCESS;
 }
