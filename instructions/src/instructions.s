7bbecc3682a43f2d9fc138c5d828fe6818fe7082
Sobhan Mohammadpour <sobhan@gentoo.org>
2018-06-15 12:48:48 +0430

    rename instruction file and remove header

--- instructions/src/instructions.s
+++ instructions/src/instructions.s
@@ -1,69 +1,43 @@
-<<<<<<< HEAD
-	area start, code
-	export StartHere
-StartHere
-	MRC p15, 0, r0, c1, c1, 2
-
-	ORR r0, r0, #2_11<<10 ; enable fpu
-
-	MCR p15, 0, r0, c1, c1, 2
-
-	LDR r0, =(0xF << 20)
-
-	MCR p15, 0, r0, c1, c0, 2
-
-	MOV r3, #0x40000000
-
-	VMSR FPEXC, r3
-
-	import __main
-	b __main
-
-	area mycode, code
-	export myasm
-
-myasm
-
 	;#dup
-  	pop {r0}
-  	push {r0}
-  	push {r0}
+	pop {r0}
+	push {r0}
+	push {r0}
 
 	;#dup2
-    pop {r0, r1}
-    push {r0, r1}
-    push {r0,r1}
+	pop {r0, r1}
+	push {r0, r1}
+	push {r0,r1}
 
-    ;#dup_x1
-    pop {r0,r1}
-    push {r0}
-    push {r0,r1}
+	;#dup_x1
+	pop {r0,r1}
+	push {r0}
+	push {r0,r1}
 
-    ;#dup_x2
-    pop {r0,r1,r2}
-    push {r0}
-    push {r0,r1,r2}
+	;#dup_x2
+	pop {r0,r1,r2}
+	push {r0}
+	push {r0,r1,r2}
 
-    ;#pop
-    pop {r0}
+	;#pop
+	pop {r0}
 
-    ;#pop_2
-    pop {r0, r1}
+	;#pop_2
+	pop {r0, r1}
 
-    ;#swap
-   	pop {r0, r1}
-   	mov r2, r0
-   	push {r1, r2}
+	;#swap
+	pop {r0, r1}
+	mov r2, r0
+	push {r1, r2}
 
 	;#dup2_x1
-    pop {r0,r1,r2}
-    push {r0,r1}
-    push{r0,r1,r2}
+	pop {r0,r1,r2}
+	push {r0,r1}
+	push{r0,r1,r2}
 
 	;#dup2_x2
-    pop  {r0,r1,r2,r3}
-    push  {r0,r1}
-    push  {r0,r1,r2,r3}
+	pop  {r0,r1,r2,r3}
+	push  {r0,r1}
+	push  {r0,r1,r2,r3}
 
 	;#fconst_0
 	vsub.f32 s0, s0, s0
@@ -125,9 +99,9 @@ myasm
 	vpush.f32 {s0}
 
 	;#fcmpg
-  	vpop.f32 {s0, s1}
-  	vcmp.f32 s0,s1
-  	VMRS APSR_nzcv, FPSCR
+	vpop.f32 {s0, s1}
+	vcmp.f32 s0,s1
+	VMRS APSR_nzcv, FPSCR
 	subeq r0,r0,r0
 	pusheq {r0}
 	movgt r0,#1
@@ -136,9 +110,9 @@ myasm
 	pushlt {r0}
 
 	;#fcmpl
-  	vpop.f32 {s0, s1}
-  	vcmp.f32 s0,s1
-  	VMRS APSR_nzcv, FPSCR
+	vpop.f32 {s0, s1}
+	vcmp.f32 s0,s1
+	VMRS APSR_nzcv, FPSCR
 	subeq r0,r0,r0
 	pusheq {r0}
 	movgt r0,#1
@@ -147,9 +121,9 @@ myasm
 	pushlt {r0}
 
 	;#dcmpg
-  	vpop.f64 {d0, d1}
-  	vcmp.f64 d0,d1
-  	VMRS APSR_nzcv, FPSCR
+	vpop.f64 {d0, d1}
+	vcmp.f64 d0,d1
+	VMRS APSR_nzcv, FPSCR
 	subeq r0,r0,r0
 	pusheq {r0}
 	movgt r0,#1
@@ -158,9 +132,9 @@ myasm
 	pushlt {r0}
 
 	;#dcmpl
-  	vpop.f64 {d0, d1}
-  	vcmp.f64 d0,d1
-  	VMRS APSR_nzcv, FPSCR
+	vpop.f64 {d0, d1}
+	vcmp.f64 d0,d1
+	VMRS APSR_nzcv, FPSCR
 	subeq r0,r0,r0
 	pusheq {r0}
 	movgt r0,#1
@@ -257,6 +231,3 @@ myasm
 	add r0, r1
 	vstr.f64 d0, [r0]
 
-	bx lr
-
-	end
