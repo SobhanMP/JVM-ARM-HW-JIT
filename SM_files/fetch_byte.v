9bed1df995c5637fc1a006ea0803217ea8f5a419
Sobhan Mohammadpour <sobhan@gentoo.org>
2018-06-20 18:27:08 +0430

    remove useless clk

--- SM_files/fetch_byte.v
+++ SM_files/fetch_byte.v
@@ -1,6 +1,6 @@
 `include "me_consts.vh"
 module fetch_byte(output wire fetch, input param_even, input [7:0] parameter_number,
-  input [`SMNL-1:0] state, input clk);
+  input [`SMNL-1:0] state);
 
   assign fetch = (state == `FETCH_INSTRUCTION) ||
     (!param_even && (|parameter_number) && (state == `FETCH_PARAMS));
--- SM_files/fetch_byte_tb.v
+++ SM_files/fetch_byte_tb.v
@@ -3,22 +3,19 @@
 module fetch_byte_tb;
 
   reg [`SMNL-1:0] state;
-  reg clk;
   wire fetch;
   reg param_even;
   reg [7:0] parameter_number;
   fetch_byte f(.fetch(fetch), .param_even(param_even),
-    .parameter_number(parameter_number), .state(state), .clk(clk));
+    .parameter_number(parameter_number), .state(state));
 
 
   initial begin
-    clk = 0;
     state = 0;
     param_even = 0;
     parameter_number = 0;
   end
 
-  always#2 clk = !clk;
   always #32 param_even = !param_even;
   always #4 state = state + 1;
   always #32 parameter_number = parameter_number + 1;
