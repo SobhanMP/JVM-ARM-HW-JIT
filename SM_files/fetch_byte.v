c3d72fc1c08b6242376ca87b0764da022afec3a3
Sobhan Mohammadpour <sobhan@gentoo.org>
2018-06-20 20:02:02 +0430

    move these file into an assign thing

--- SM_files/fetch_byte.v
+++
@@ -1,8 +0,0 @@
-`include "me_consts.vh"
-module fetch_byte(output wire fetch, input param_even, input [7:0] parameter_number,
-  input [`SMNL-1:0] state);
-
-  assign fetch = (state == `FETCH_INSTRUCTION) ||
-    (!param_even && (|parameter_number) && (state == `FETCH_PARAMS));
-
-endmodule
